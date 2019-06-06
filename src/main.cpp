ec1c2f5cae4e219b937fe4bda21f6533b993b875
ArinaGorbacheva <43599396+ArinaGorbacheva@users.noreply.github.com>
2019-06-07 02:18:43 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,24 +3,24 @@
 int main() {
 
 	int n;
-	prefix pref;
-	vector<string> vec;
-	map<prefix, vector<string> > statetab;
+	prefix pref; //тек вводимый префикс
+	vector<string> vec; //суффиксы
+	map<prefix, vector<string> > statetab; //префикс-суффиксы
 	string str;
-	vector<string> p;
-	ifstream fin("input.txt");
+	prefix p;
+	ifstream fin("input.txt"); //файл с исходным текстом
 
-	if (fin.is_open()) {
+	if (fin.is_open()) { //считываем файл
 		for (int i = 0; i < NPREF; i++) {
-			fin >> str;
+			fin >> str; 
+			pref.push_back(str); //запись префикса
 			p.push_back(str);
-			pref.push_back(str);
 		}
 		while (fin >> str) {
 			if (statetab.count(pref) == 0) {
-				vec.push_back(str);
-				statetab.insert(pair<prefix, vector<string>>(pref, vec));
-				vec.clear();
+				vec.push_back(str); //запись суффикса
+				statetab.insert(pair<prefix, vector<string>>(pref, vec)); //помещаем полученные  префф-суфф в statetab
+				vec.clear(); 
 			}
 			else {
 				statetab[pref].push_back(str);
@@ -33,20 +33,20 @@ int main() {
 		cout << "File is not corrected!" << endl;
 	}
 	fin.close();
-	pref.clear();
+	pref.clear(); 
 
 	for (int i = 0; i < NPREF; i++) {
-		pref.push_back(p[i]);
+		pref.push_back(p[i]); 
 	}
 
 	ofstream out("output.txt");
 	srand(time(0));
 	out << pref[0] << " " << pref[1];
 	for (int i = 0; i < MAXGEN - 2; i++) {
-		if (statetab.count(pref)) {
-			n = rand() % statetab[pref].size();
-			out << statetab[pref][n] << " ";
-			if ((i + 1) % 10 == 0) out << endl;
+		if (statetab.count(pref)) { //берем префф 
+			n = rand() % statetab[pref].size(); //для него выбирается случайный суфф
+			out << statetab[pref][n] << " "; 
+			if ((i + 1) % 10 == 0) out << endl; //разделить строки между собой
 			pref.push_back(statetab[pref][n]);
 			pref.pop_front();
 
@@ -54,7 +54,5 @@ int main() {
 		else break;
 	}
 	out.close();
-
 	return 0;
-
 }
